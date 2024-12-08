3078de1faac51bff1715ae8b5c049831ff52bc9d
OlegEfremov54 <ole_f@mail.ru>
2024-12-08 22:05:56 +0300

    Rjvbn 1

---
+++ app/src/main/java/com/example/dz14frag2/SecFragment.kt
@@ -0,0 +1,25 @@
+package com.example.dz14frag2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class SecFragment : Fragment() {
+
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sec, container, false)
+    }
+
+
+
+}
